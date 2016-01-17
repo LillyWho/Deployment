20296fd4c77dfb33186756725e4e6f0680b75a2d
Alexandroid99 <Alexander@GLaDOS.fritz.box>
2016-01-17 21:01:03 +0100

    Added improved startup script
    Features:
    Forces server to start again and verify/update files after quitting.
    Useful in combination with the updater plugin.

---
+++ modules/tf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+trap "echo 'Server stopped.'" SIGINT
+while :; do
+	tf2/srcds_run -game tf -autoupdate +sv_pure 0 +randommap +maxplayers 24 -replay -steam_dir /home/teamfortress/ -steamcmd_script /home/teamfortress/tf2_ds.txt +sv_shutdown_timeout_minutes 360
+	read -p "Press enter to resume the server, enter quit then press enter to terminate screen..."$'\n' input
+	if [[ "$input" = quit ]]; then
+		break
+	fi
+done
